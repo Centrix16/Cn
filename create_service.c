cd47d5f7c0bc67e563e6298b395b8a2508bf37c5
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-03 17:08:34 +0300

    Reduced to a general view

--- create_service.c
+++ create_service.c
@@ -1,20 +1,18 @@
  /*
   * Creature a file
   * v0.2
-  * 01.04.2004
-  * Nifra -- %TAS%
-*/
+  * 01.04.2020
+  * Nifra -- TAS
+  */
 
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	if (argc != 2) {
-		fprintf(stderr, "%s: 1 argument required\n", argv[0]);
+		fprintf(stderr, "%s: %s <fname>\n", argv[0], argv[0]);
 		return 1;
 	}
-	FILE *fp;
-	fp = fopen(argv[1], "a");
-	fclose(fp);
+
+	fclose(fopen(argv[1], "w"));
 	return 0;
 }

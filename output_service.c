ebe8ac1d094b47b7c0edb42013d00fa88adc1f3c
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-03 17:09:14 +0300

    Reduced to a general view

--- output_service.c
+++ output_service.c
@@ -1,27 +1,29 @@
 /*
  * File output to the screen
- * v0.1
+ * v0.2
  * 02.04.2020
  * Nifra -- TAS
  */
 
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	FILE *fp = NULL;
+	char ch = (char)0;
+
 	if (argc != 2) {
-		fprintf(stderr, "%s: 1 argument required\n", argv[0]);
+		fprintf(stderr, "%s: %s <fname>\n", argv[0], argv[0]);
 		return 1;
 	}
-	FILE *fp;
 	if ((fp = fopen(argv[1], "r")) == NULL) {
-		fprintf(stderr, "Can not open the %s file\n", argv[1]);
+		fprintf(stderr, "Can not open the `%s` file\n", argv[1]);
 		return 1;
 	}
-	char ch;
+
 	while ((ch = getc(fp)) != EOF) {
 		putchar(ch);
 	}
 	fclose(fp);
+
 	return 0;
 }

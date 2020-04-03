000e07d7d686f26ce5b5dfaf22d09972cd48e396
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-03 17:10:17 +0300

    Reduced to a general view

--- remove_service.c
+++ remove_service.c
@@ -1,21 +1,18 @@
  /*
   * Remove a file
   * v0.2
-  * 01.04.2004
-  * Nifra -- %TAS%
-*/
+  * 01.04.2020
+  * Nifra -- TAS
+  */
 
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	if (argc != 2) {
-		fprintf(stderr, "%s: 1 argument required\n", argv[0]);
+		fprintf(stderr, "%s: %s <fname>\n", argv[0], argv[0]);
 		return 1;
 	}
-	if (remove(argv[1]) == -1) {
-		fprintf(stderr, "Can not delete %s\n", argv[1]);
-	}
-	perror("remove");
+	if (remove(argv[1]) == -1)
+		fprintf(stderr, "Can`t delete %s\n", argv[1]);
 	return 0;
 }

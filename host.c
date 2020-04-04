bde4b55f13fdf70fc4ee96cc836d49109d87ce84
Centrix16 <tel1tsyn@yandex.ru>
2020-04-04 17:12:36 +0300

    add host

---
+++ host.c
@@ -0,0 +1,16 @@
+/*
+ * Host for Cn
+ * v0.1
+ * 04.04.2020
+ * Nifra -- TAS
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	/* code */
+	return 0;
+}

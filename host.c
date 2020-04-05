7216e4bbb229d54c2539d998cf81bfe305462e62
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-05 15:16:06 +0300

    The first version of the host was written

--- host.c
+++ host.c
@@ -1,16 +1,48 @@
-/*
- * Host for Cn
- * v0.1
- * 04.04.2020
- * Nifra -- TAS
- */
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-int main(int argc, char *argv[])
-{
-	/* code */
+#include "coniox/coniox.h"
+#include "host.h"
+
+#define output(...) {mvCursor(X_START, Y_START+line); printf(__VA_ARGS__); line++;}
+
+char *get_buf_name(char *fname);
+void get_file_info(char *fname);
+
+int line = 0;
+
+int main(int argc, char *argv[]) {
+	char cmd[256] = "";	
+
+	if (argc < 2) {
+		fprintf(stderr, "%s: %s <fname>\n", argv[0]);
+		return 0;
+	}
+
+	clean();
+
+	/* buffer creation */
+
+	// sprintf(cmd, "%s/%s %s %d", SERV_DIR, OUTPUT_SERVICE, get_buf_name(argv[1]), COL_MAX);
+	sprintf(cmd, "%s/%s %s %d", SERV_DIR, OUTPUT_SERVICE, argv[1], COL_MAX);
+	system(cmd);
+
+	get_file_info(argv[1]);
 	return 0;
+}
+
+char *get_buf_name(char *fname) {
+	return strcat(fname, "_buf");
+}
+
+void get_file_info(char *fname) {
+	struct stat buf;
+
+	stat(fname, &buf);
+
+	output("NAME: %s", fname);
+	output("SIZE: %d", buf.st_size);
+	output("UOID: %d", buf.st_uid);
 }

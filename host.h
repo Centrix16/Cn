25f958ac4915c0f69b2813f68bc31e5fabbdb4c4
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-05 19:10:31 +0300

    added new defenitions

--- host.h
+++ host.h
@@ -6,8 +6,15 @@
 #define OUTPUT_SERVICE "os"
 
 #define COL_MAX 64
+#define COL_END 100
+#define USER_INPUT_DEFAULT_COL_OFFSET 4 
+#define USER_INPUT_DEFAULT_ROW 7
 
 #define X_START 68
 #define Y_START 3
+
+#define ESC 27
+#define NL 10
+#define BS 127 
 
 #endif

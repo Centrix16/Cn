454388d709319eda78cefd9255273f2c0f5adb2e
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-05 15:17:25 +0300

    Added a header to host.c

---
+++ host.h
@@ -0,0 +1,13 @@
+#ifndef __CN_TEXT_EDITOR_HOST_H_INCLUDED__
+#define __CN_TEXT_EDITOR_HOST_H_INCLUDED__
+
+#define SERV_DIR "bin"
+
+#define OUTPUT_SERVICE "os"
+
+#define COL_MAX 64
+
+#define X_START 68
+#define Y_START 3
+
+#endif

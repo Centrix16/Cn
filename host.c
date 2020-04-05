105d815d568b62222a20d1552ac45c71bed4f77f
Centrix14 <artiom.zvyagin@yandex.ru>
2020-04-05 19:09:08 +0300

    New version 0.2

--- host.c
+++ host.c
@@ -1,6 +1,6 @@
 /*
  * host -- combines services together
- * v0.1
+ * v0.2
  * 05.04.2020
  * Nifra -- ASZ
  */
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/stat.h>
 
 #include "coniox/coniox.h"
@@ -17,8 +18,13 @@
 
 char *get_buf_name(char *fname);
 void get_file_info(char *fname);
+void navigation();
+void input();
 
+int x = 1, y = 1;
 int line = 0;
+
+char input_buf[1024];
 
 int main(int argc, char *argv[]) {
 	char cmd[256] = "";	
@@ -37,6 +43,8 @@ int main(int argc, char *argv[]) {
 	system(cmd);
 
 	get_file_info(argv[1]);
+	navigation();
+	clean();
 	return 0;
 }
 
@@ -52,4 +60,110 @@ void get_file_info(char *fname) {
 	output("NAME: %s", fname);
 	output("SIZE: %d", buf.st_size);
 	output("UOID: %d", buf.st_uid);
+	output("LMOD: %s", ctime(&buf.st_ctime));
+	output("CPOS: %d-%d", x, y);
+
+	output("USER INPUT");
+	output("%s", input_buf);
+}
+
+void navigation() {
+	int c = 0;
+
+	while (c != ESC) {
+		switch (c) {
+			case 'h':
+			case 'a':
+				if (x > 0)
+					x--;	
+			break;
+
+			case 'j':
+			case 's':
+				y++;
+			break;
+
+			case 'k':
+			case 'w':
+				if (y > 0)
+					y--;	
+			break;
+
+			case 'l':
+			case 'd':
+				if (x < COL_MAX)
+					x++;	
+			break;
+
+			default:
+				if (c == 'i')
+					input();	
+			break;
+		}
+
+		mvCursor(X_START, Y_START+4);
+		printf("CPOS: %d-%d", x, y);
+
+		mvCursor(x, y);
+		c = readKb();
+	}
+}
+
+int is_valid_c(int c) {
+	if ((isprint(c) || isspace(c)) && c != '\n') return 1;
+
+	return 0;
+}
+
+void input() {
+	int c = 0, i = 0, col = USER_INPUT_DEFAULT_COL_OFFSET, row = USER_INPUT_DEFAULT_ROW;
+
+	while (i < 1024 && c != ESC) {
+		switch (c) {	
+			case NL:
+				row++;
+				col = USER_INPUT_DEFAULT_COL_OFFSET;
+				input_buf[i++] = '\n';
+
+				mvCursor(X_START+col, Y_START+row);
+			break;
+
+			case BS:
+				if (X_START+(col-1) == X_START+USER_INPUT_DEFAULT_COL_OFFSET-1) {
+					row--;
+					col = COL_END-X_START;
+				}
+
+				if (row > USER_INPUT_DEFAULT_ROW-1) {
+					input_buf[--i] = 0;
+					mvCursor(X_START+(--col), Y_START+row);
+					putchar(' ');
+					mvCursor(X_START+col, Y_START+row);
+				}
+			break;
+
+			default:		
+				if (is_valid_c(c)) {
+					input_buf[i++] = c;
+
+					if ((X_START+col) == COL_END) {
+						row++;
+						col = USER_INPUT_DEFAULT_COL_OFFSET;
+					}
+
+					mvCursor(X_START+col++, Y_START+row);
+
+					if (isupper(c))
+						setColor(BLACK, RED, BRIGHT);
+					else
+						setColor(BLACK, WHITE, BRIGHT);
+					putchar(c);
+				}
+			break;
+		}
+
+		c = readKb();
+	}
+
+	setColor(BLACK, WHITE, NORMAL);
 }

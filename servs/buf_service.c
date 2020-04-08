04d0568cdb38d6fc18bba52e8dadfe9a9cbad1be
Centrix16 <tel1tsyn@yandex.ru>
2020-04-08 19:15:35 +0300

    small changes

--- servs/buf_service.c
+++ servs/buf_service.c
@@ -1,7 +1,7 @@
 /*
  * Creates a file-related buffer
- * v0.1
- * 06.04.2020
+ * v0.2
+ * 08.04.2020
  * Nifra -- TAS
  */
 
@@ -10,19 +10,35 @@
 
 int main(int argc, char *argv[])
 {
+	printf("hello\n");
 	if (argc != 2) {
 		fprintf(stderr, "%s: %s <bufname>\n", argv[0], argv[0]);
 		return 1;
 	}
 	FILE *fp, *buf;
-	char *bufname = argv[1];
-       	strcat(bufname, "_buf");
-	fp = fopen(argv[1], "r");
-	buf = fopen(bufname, "w");
+	char *bufname;
+	sprintf(bufname, "%s_buf", argv[1]);
+
+	printf("%s\n", bufname);
+	if ((fp = fopen(argv[1], "r")) != NULL) {
+		printf("hi");
+		return 1;
+	}
+	printf("hello\n");
+	perror("fopen");
+
+	if ((buf = fopen(bufname, "w")) == NULL) {
+		fprintf(stderr, "s: can not open %s file", argv[0], bufname);
+		return 1;
+	}
+	printf("hello\n");
+
 	char ch;
 	while ((ch = fgetc(fp)) != EOF) {
 		fputc(ch, buf);
 	}
+	printf("hello\n");
+
 	fclose(fp);
 	fclose(buf);
 	return 0;
